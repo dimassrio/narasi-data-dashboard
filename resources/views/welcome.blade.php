9be8335ba580c6e4260e677bf236927053644ef9
Dimas Satrio <dimassrio@gmail.com>
2019-01-04 10:01:07 +0700

    fixing master

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css"> -->
+
+        <!-- Styles -->
+        <style>
+            html, body{
+				font-size: 16px;
+				padding: 0;
+				margin: 0;
+			}
+			iframe{
+				width: 100%;
+			}
+        </style>
+    </head>
+    <body>
+        <iframe height="4000" src="https://datastudio.google.com/embed/reporting/1gxRuX6KmXgnXnNVRtDJ7Q5SXYuoPzJV0/page/jwsZ" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </body>
+</html>
