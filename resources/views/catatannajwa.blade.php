c621a932ff232b0eac6708d466fdc06a3704328d
Dimas Satrio <dimassrio@gmail.com>
2019-01-15 13:03:41 +0700

    fixing master

---
+++ resources/views/catatannajwa.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Catatan Najwa At Narasi</title>
+
+        <!-- Fonts -->
+        <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css"> -->
+
+        <!-- Styles -->
+        <style>
+            html, body{
+				font-size: 16px;
+				padding: 0 10px;
+				margin: 0;
+			}
+			iframe{
+				width: 100%;
+			}
+        </style>
+    </head>
+    <body>
+        <iframe id="iframe" height="3830" src="https://datastudio.google.com/embed/reporting/1enY3agv7MA8bsUeNQsyrfmuKs1hTWxn6/page/HgPf" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </body>
+	<script>
+		// document.querySelector("#iframe").onload = function(){
+		// 	console.log('iframe');
+		// 	console.log(document.querySelector("#iframe"));
+		// }
+		
+	// document.addEventListener("DOMContentLoaded", function(event) { 
+		//do work
+	// });
+		// $(document).ready(function(params) {
+		// 	$("#iframe").contents().find('.embedding-footer-bg').hide();
+		// 	console.log($("#iframe"));
+		// })
+	</script>
+</html>
