240f5b7d99fdbf186c0c47efefe4a412f8881563
Dimas Satrio <dimassrio@gmail.com>
2019-02-07 12:22:36 +0700

    fixing data

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -7,8 +7,10 @@ use App\Statistic;
 class DataController extends Controller
 {
     public function getView(Request $request, $sub){
+		if($sub == 'data'){
+			return view('data');
+		}
 		$data = Statistic::where('slug', $sub)->get()->first();
-
 		return view('statistics', compact('data'));
 	}
 }
